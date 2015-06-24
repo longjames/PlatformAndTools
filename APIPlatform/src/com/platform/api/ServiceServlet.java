ee9da32c8d77bbe22b683319f762b07e9abf8702
Carpela <carpela@163.com>
2015-06-24 15:29:02 +0800

    Hibernate Models and JSON API Servlet

---
+++ APIPlatform/src/com/platform/api/ServiceServlet.java
@@ -0,0 +1,146 @@
+package com.platform.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.platform.model.Shouhuan;
+import com.platform.model.*;
+
+/**
+ * Servlet implementation class ServiceServlet
+ */
+@WebServlet("/service")
+public class ServiceServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServiceServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String type = request.getParameter("service_type");
+		System.out.println("Service: "+type);
+		response.setContentType("text/x-json");
+		
+		PrintWriter out = response.getWriter();
+		Map<String, String> data = new HashMap<String, String>();
+		
+		if(type==null || type.equals(""))
+		{
+			data.put("code","200");
+			data.put("msg", "获取数据失败");
+			data.put("data", "");
+			out.println(JSONObject.fromObject(data).toString());
+			return;
+		}
+		Integer type_i = Integer.valueOf(type);
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session s = sf.openSession();
+	
+		try{
+			SQLQuery query = s.createSQLQuery("select * from service where service_type="+type_i).addEntity(Service.class);
+			Service ser = (Service) query.uniqueResult();
+			
+			data.put("code","100");
+			data.put("msg", "获取数据成功");
+			data.put("data", JSONObject.fromObject(ser).toString());
+			
+			out.println(JSONObject.fromObject(data).toString());
+		}catch(Exception e)
+		{
+			data.put("code","200");
+			data.put("msg", "获取数据失败");
+			data.put("data", "");
+			e.printStackTrace();
+			out.println(JSONObject.fromObject(data).toString());
+		}finally
+		{
+			s.close();
+			sf.close();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String desc = request.getParameter("describle");
+		Integer type = 0;
+		Integer price = 0;
+		Integer ime = 0;
+		
+		try{
+			type = Integer.valueOf(request.getParameter("service_type"));
+			price = Integer.valueOf(request.getParameter("service_price"));
+			ime = Integer.valueOf(request.getParameter("workime"));
+		}catch(NumberFormatException e)
+		{
+			e.printStackTrace();
+		}
+		
+		System.out.println("Service: "+desc+" "+type+" "+price+" "+ime);
+		response.setContentType("text/x-json");
+		
+		PrintWriter out = response.getWriter();
+		Map<String, String> data = new HashMap<String, String>();
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session s = sf.openSession();
+		Transaction t = s.beginTransaction();
+		
+		try{
+			
+			Service service = new Service();
+			service.setService_describe(desc);
+			service.setService_price(price);
+			service.setService_type(type);
+			service.setService_workime(ime);
+			
+			s.save(service);
+			t.commit();
+			
+			data.put("code","100");
+			data.put("msg", "添加数据成功");
+			data.put("data", "");
+			
+			out.println(JSONObject.fromObject(data).toString());
+		}catch(Exception e)
+		{
+			t.rollback();
+			data.put("code","200");
+			data.put("msg", "添加数据失败");
+			data.put("data", "");
+			e.printStackTrace();
+			out.println(JSONObject.fromObject(data).toString());
+		}finally
+		{
+			s.close();
+			sf.close();
+		}
+	}
+
+}
