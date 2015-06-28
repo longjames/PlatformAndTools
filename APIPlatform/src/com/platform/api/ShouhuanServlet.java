a473641cbf8aa1fbe63a4cfa655f0861a87af41c
Carpela <carpela@163.com>
2015-06-28 16:44:08 +0800

    update for all models

--- APIPlatform/src/com/platform/api/ShouhuanServlet.java
+++ APIPlatform/src/com/platform/api/ShouhuanServlet.java
@@ -1,6 +1,8 @@
 package com.platform.api;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
@@ -157,6 +159,74 @@ public class ShouhuanServlet extends HttpServlet {
 	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		BufferedReader in = new BufferedReader(new InputStreamReader(request.getInputStream()));  
+	    String line;  
+	    StringBuilder sb = new StringBuilder();
+	    while ((line = in.readLine()) != null)
+	    {  
+//	        System.out.println(line);
+	        sb.append(line);
+	    }
+	    System.out.println(sb.toString());
+	    JSONObject jo = JSONObject.fromObject(sb.toString());
+	    String id = null;
+	    String which = null;
+	    String value = null;
+	    response.setContentType("text/x-json");
+		
+		PrintWriter out = response.getWriter();
+		Map<String, String> data = new HashMap<String, String>();
+	    try{
+	    	id = jo.getString("shouhuan_id");
+			which = jo.getString("which");
+			value = jo.getString("value");
+	    }catch(Exception e)
+	    {
+	    	data.put("code","200");
+			data.put("msg", "修改数据失败");
+			data.put("data", "");
+			e.printStackTrace();
+			out.println(JSONObject.fromObject(data).toString());
+			return;
+	    }
+		
+		System.out.println("Shouhuan(update): "+which+"->"+value);
+		
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session s = sf.openSession();
+		Transaction t = s.beginTransaction();
+		
+		try{
+			String sql = "update shouhuan set "+which+" = "+value+" where shouhuan_id ="+id;
+			System.out.println(sql);
+			SQLQuery query = s.createSQLQuery(sql);
+			query.addEntity(Shouhuan.class);
+//			SQLQuery query = s.createSQLQuery("update user set ? = ? where user_id = ?");
+//			query.setParameter(0, which);
+//			query.setParameter(1, value);
+//			query.setParameter(2, id);
+			query.executeUpdate();
+			t.commit();
+			
+			data.put("code","100");
+			data.put("msg", "删除数据成功");
+			data.put("data", "");
+			
+			out.println(JSONObject.fromObject(data).toString());
+			
+		}catch(Exception e)
+		{
+			data.put("code","200");
+			data.put("msg", "修改数据失败");
+			data.put("data", "");
+			e.printStackTrace();
+			out.println(JSONObject.fromObject(data).toString());
+		}
+		finally
+		{
+			s.close();
+			sf.close();
+		}
 	}
 
 	/**
